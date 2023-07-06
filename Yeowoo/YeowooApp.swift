dccd18ff82eb64eb1b3b81de24931ffa35cf7cd6
sunghun <leedool3003@gmail.com>
2023-07-06 23:18:43 +0900

    :tada: [init] init Yeowoo project #2

---
+++ Yeowoo/YeowooApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeowooApp.swift
+//  Yeowoo
+//
+//  Created by Kim SungHun on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct YeowooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
