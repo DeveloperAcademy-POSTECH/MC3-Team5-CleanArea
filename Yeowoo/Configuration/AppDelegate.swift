d02bab57e8520c03ba465543fe2a9adcacdd140c
sunghun <leedool3003@gmail.com>
2023-07-13 17:19:50 +0900

    :hammer: [chore] FCM 기본 세팅 #14

---
+++ Yeowoo/Configuration/AppDelegate.swift
@@ -0,0 +1,65 @@
+//
+//  AppDelegate.swift
+//  Yeowoo
+//
+//  Created by Kim SungHun on 2023/07/13.
+//
+
+import UIKit
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		
+		FirebaseApp.configure()
+		
+		UNUserNotificationCenter.current().delegate = self
+		
+		let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+		UNUserNotificationCenter.current().requestAuthorization(
+			options: authOptions,
+			completionHandler: { _, _ in }
+		)
+		
+		application.registerForRemoteNotifications()
+		
+		Messaging.messaging().delegate = self
+		
+		// 푸시 포그라운드 설정
+		UNUserNotificationCenter.current().delegate = self
+		
+		return true
+	}
+	
+	// fcm 토큰이 등록 되었을 때
+	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+		Messaging.messaging().apnsToken = deviceToken
+	}
+}
+
+extension AppDelegate: MessagingDelegate {
+	
+	// fcm 등록 토큰을 받았을 때
+	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+		print("get Firebase FCM token \(String(describing: fcmToken))")
+	}
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+	
+	// foreground
+	func userNotificationCenter(_ center: UNUserNotificationCenter,
+								willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+		
+		let userInfo = notification.request.content.userInfo
+		print("willPresent: userInfo ", userInfo)
+		return [.banner, .sound, .badge]
+	}
+	
+	// 메시지를 받았을 때
+	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+		let userInfo = response.notification.request.content.userInfo
+		print("didReceive: userInfo ", userInfo)
+	}
+}
--- Yeowoo/Sources/ViewModels/TestViewModel.swift
+++ Yeowoo/Sources/ViewModels/TestViewModel.swift
@@ -14,20 +14,20 @@ final class TestViewModel: ObservableObject {
 	
 	private var cancellables = Set<AnyCancellable>()
 
-	func fetchUser() {
-		print("call fetchUser")
-		FirebaseService.fetchTest()
-			.sink { completion in
-				switch completion {
-				case .failure(let error):
-					print(error.localizedDescription)
-				case.finished:
-					return
-				}
-			} receiveValue: { [weak self] tests in
-				self?.tests = tests
-				print("tests = \(self?.tests ?? [])")
-			}
-			.store(in: &cancellables)
-	}
+//	func fetchUser() {
+//		print("call fetchUser")
+//		FirebaseService.fetchTest()
+//			.sink { completion in
+//				switch completion {
+//				case .failure(let error):
+//					print(error.localizedDescription)
+//				case.finished:
+//					return
+//				}
+//			} receiveValue: { [weak self] tests in
+//				self?.tests = tests
+//				print("tests = \(self?.tests ?? [])")
+//			}
+//			.store(in: &cancellables)
+//	}
 }
