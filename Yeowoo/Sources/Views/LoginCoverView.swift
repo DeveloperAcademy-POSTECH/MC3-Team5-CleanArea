fd42dd77e1d0e33367cec7214c885cf5146a371f
Jisull <128975136+Jisull@users.noreply.github.com>
2023-07-17 02:15:05 +0900

    ✨ [feat] 로그인 뷰 구현 #18

--- Yeowoo/Sources/Views/LoginCoverView.swift
+++ Yeowoo/Sources/Views/LoginCoverView.swift
@@ -9,38 +9,37 @@ import SwiftUI
 
 struct LoginCoverView: View {
     var body: some View {
-        VStack{
-            Image("여우")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 244, height: 119.29)
-            Image("logincover")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 350, height: 240)
-            
-            Button {
-                //move to LoginView
-            } label: {
-                Text("로그인")
-                    .foregroundColor(.white)
-                    .frame(width: 350, height: 54)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color("B1")))
-                        }
-            Button {
-                //move to SignupView
-            } label: {
-                Text("회원가입")
-                    .foregroundColor(.black)
-                    .frame(width: 350, height: 54)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color("G6")))
-                        }
+        NavigationView {
+            VStack{
+                Image("여우")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 244, height: 119.29)
+                Image("logincover")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 240)
+
+                NavigationLink(destination: LoginView()) {
+                    Text("로그인")
+                        .foregroundColor(.white)
+                        .frame(width: 350, height: 54)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color("B1")))
+                }.navigationTitle("")
+                
+                NavigationLink(destination: SignUpView()) {
+                    Text("회원가입")
+                        .foregroundColor(.black)
+                        .frame(width: 350, height: 54)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color("G6")))
+                }.navigationTitle("")
+                
+            }
         }
-        
     }
 }
 
--- Yeowoo/Sources/Views/LoginView.swift
+++ Yeowoo/Sources/Views/LoginView.swift
@@ -8,10 +8,92 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var ID = ""
+    @State private var password = ""
+    
+    @State var isLoggedIn: Bool = false
+    @State var isShownPassword: Bool = false
+    
+    @State private var isAutoLogin = false
+    @State private var isSavedID = false
+    
     var body: some View {
-       
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        NavigationView{
+            VStack{
+                
+                Text("로그인")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding()
+                
+                //아이디 입력창
+                TextField("아이디", text: $ID)
+                    .padding()
+                    .frame(width: 350, height: 46)
+                    .background(Color("G5"))
+                    .cornerRadius(10)
+                
+                //비밀번호 입력창
+                ZStack(alignment: .trailing) {
+                    if isShownPassword{
+                        SecureField("비밀번호", text: $password)
+                            .padding()
+                            .frame(width: 350, height: 46)
+                            .background(Color("G5"))
+                            .cornerRadius(10)
+                    } else { TextField("비밀번호", text: $password)
+                            .padding()
+                            .frame(width: 350, height: 46)
+                            .background(Color("G5"))
+                            .cornerRadius(10)
+                    }
+                    //입력시 비밀번호 보이기 및 감추기
+                    Button {
+                        //수정~~~~~~~~~~~~~~~
+                        isShownPassword.toggle()
+                    } label: {
+                        Image(systemName: isShownPassword ? "eye" : "eye.slash.fill")
+                            .foregroundColor(Color("G3"))
+                    }
+                    .padding()
+                }
+//                }
+                
+                //자동로그인 및 아이디 저장 체크박스
+                HStack {
+                    Toggle("자동 로그인", isOn: $isAutoLogin)
+                        .toggleStyle(CheckboxToggleStyle())
+                    Toggle("아이디 저장", isOn: $isSavedID)
+                        .toggleStyle(CheckboxToggleStyle())
+                }
+                
+                Spacer()
+                
+                //로그인 버튼
+                Button("로그인"){
+                    //Authenticate user
+                }
+                .foregroundColor(.white)
+                .frame(width: 350, height: 54)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                    .fill(Color("B1")))
+            }.padding()
+        }
+    }
+}
+
+//자동로그인 및 아이디 저장 토글
+struct CheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: {configuration.isOn.toggle() }) {
+            HStack {
+                Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle.fill")
+                        .foregroundColor(configuration.isOn ? .black : Color("G5"))
+                configuration.label
+                        .foregroundColor(Color("G2"))
+                     }
+        }
     }
 }
 
