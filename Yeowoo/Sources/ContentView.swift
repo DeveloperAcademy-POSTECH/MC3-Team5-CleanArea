f0471de8376394f1336d5b4f83bee1955e9e596c
sunghun <leedool3003@gmail.com>
2023-07-16 15:21:20 +0900

    :sparkles: [feat] 앨범 더보기 메뉴 추가 #16

--- Yeowoo/Sources/ContentView.swift
+++ Yeowoo/Sources/ContentView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("테스트")
-        }
-        .padding()
-    }
+	var body: some View {
+		NavigationView {
+			NavigationLink("테스트") {
+				AlbumView(albumDocId: "T9eJMPQEGQClFHEahX6r")
+					.navigationBarTitle("타이틀")
+					.navigationBarItems(
+						trailing:
+							Menu {
+								Button {
+									print("앨범 제목 수정하기")
+								} label: {
+									Label("앨범 제목 수정하기", systemImage: "pencil")
+								}
+								Button(role: .destructive) {
+									print("여행 종료하기")
+								} label: {
+									Label("여행 종료하기", systemImage: "xmark.circle")
+								}
+							}
+						label: {
+							Image(systemName: "plus")
+						}
+					)
+			}
+			.navigationTitle("")
+		}
+	}
 }
