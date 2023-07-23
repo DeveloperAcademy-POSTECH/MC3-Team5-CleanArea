2f3ba8bd044e65938637501f85f0a924f0fabe50
Pin <ppyj48625@naver.com>
2023-07-23 22:16:00 +0900

    🔨 [chore] modify Album Model #39

--- Yeowoo/Sources/Models/Album.swift
+++ Yeowoo/Sources/Models/Album.swift
@@ -9,22 +9,24 @@ import Foundation
 
 // 전부 다 ? 붙이는게 좋음
 
-struct Album {
-	var albumTitle: String // 앨범 제목
-	var albumCoverImage: String // 앨범 커버 url
-	var startTime: String // 여행 시작 시간
-	var finishTime: String // 여행 끝난 시간
-	var images: [ImagesEntity]
-	var isClosed: Bool
-	var users: [String]
+struct Album: Identifiable, Hashable {
+    var id: String // album DocId
+    var albumTitle: String // 앨범 제목
+    var albumCoverImage: String // 앨범 커버 url
+    var startDay: String // 여행 시작 시간
+    var endDay: String // 여행 끝난 시간
+    var images: [ImagesEntity]
+    var isClosed: Bool
+    var users: [String]
 }
 
-struct ImagesEntity {
-	let comment: String	// 설명
-	let fileName: String	// storage 파일 이름
-	let url: String	// url
-	let uploadUser: String	// 업로드 한 유저
-	let roleCheck: Bool	// 역할 확인
-	let likeUsers: [String] // 좋아요 한 유저들
-	let uploadTime: String // 올린 시간 (수정한 시간)
+struct ImagesEntity: Hashable {
+    let comment: String    // 설명
+    let fileName: String    // storage 파일 이름
+    let url: String    // url
+    let uploadUser: String    // 업로드 한 유저
+    let roleCheck: Bool    // 역할 확인
+    let likeUsers: [String] // 좋아요 한 유저들
+    let uploadTime: String // 올린 시간 (수정한 시간)
 }
+
