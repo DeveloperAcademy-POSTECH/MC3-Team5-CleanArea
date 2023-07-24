2217c3412c32e97ea05fb7a8ea6478f9fbf82569
sunghun <leedool3003@gmail.com>
2023-07-24 14:40:24 +0900

    :recycle: [refactor] AlbumDetailView 코드 정리 #35

--- Yeowoo/Sources/Views/Album/AlbumDetailView.swift
+++ Yeowoo/Sources/Views/Album/AlbumDetailView.swift
@@ -9,102 +9,45 @@ import SwiftUI
 
 struct AlbumDetailView: View {
 	
-	@Environment(\.dismiss) var dismiss
-	
 	enum AlertType: Identifiable {
 		case save, edit, remove
-		
 		var id: Int {
 			switch self {
-			case .save:
-				return 0
-			case .edit:
-				return 1
-			case .remove:
-				return 2
+			case .save: return 0
+			case .edit: return 1
+			case .remove: return 2
 			}
 		}
 	}
 	
-	var entitys: ImagesEntity
-	var user: User
+	@Environment(\.dismiss) var dismiss
 	
-	@State var testBool: Bool
-	@State var testCount: Int
+	private var entitys: ImagesEntity
+	private var user: User
 	
+	@ObservedObject private var viewModel: AlbumViewModel
+	
+	@State private var tempLikeState: Bool
+	@State private var tempLikeCount: Int
 	@State private var alertType: AlertType? = nil
 	
-	@ObservedObject var viewModel: AlbumViewModel
+	init(entitys: ImagesEntity, user: User,
+		 tempLikeState: Bool, tempLikeCount: Int, viewModel: AlbumViewModel) {
+		self.entitys = entitys
+		self.user = user
+		self._tempLikeState = State(initialValue: tempLikeState)
+		self._tempLikeCount = State(initialValue: tempLikeCount)
+		self.viewModel = viewModel
+	}
 	
 	var body: some View {
 		VStack(alignment: .leading) {
-			HStack() {
-				AsyncImage(url: URL(string: user.profileImage)) { image in
-					image
-						.resizable()
-						.frame(width: 32, height: 32)
-						.clipShape(Circle())
-				} placeholder: {
-					ProgressView()
-						.frame(width: 32, height: 32)
-				}
-				Text(user.nickname)
-				Spacer()
-			}
-			.padding(.horizontal, 20)
-			AsyncImage(url: URL(string: entitys.url)) { image in
-				image
-					.resizable()
-					.frame(height: 500)
-					.cornerRadius(20)
-					.overlay {
-						HStack(alignment: .bottom) {
-							Divider().opacity(0)
-							Spacer()
-							VStack {
-								Circle()
-									.fill(Color.white)
-									.opacity(0.25)
-									.frame(width: 48, height: 48)
-									.overlay {
-										Button {
-											Task {
-												try await viewModel.actionLike(toggleChk: testBool,
-																			   albumDocId: viewModel.albums.id,
-																			   fileName: entitys.fileName)
-												testCount = testBool ? testCount - 1 : testCount + 1
-												testBool.toggle()
-											}
-										} label: {
-											Image(systemName: "heart.fill")
-												.foregroundColor(testBool ? Color.red : Color.white)
-										}
-									}
-								Rectangle()
-									.fill(Color.white)
-									.opacity(0.25)
-									.frame(width: 48, height: 24)
-									.cornerRadius(100)
-									.overlay {
-										Text("\(testCount)")
-											.font(.system(size: 16, weight: .medium))
-											.foregroundColor(Color.white)
-									}
-							}
-						}
-						.padding(.horizontal)
-						.padding(.bottom, 20)
-					}
-			} placeholder: {
-				ProgressView()
-					.frame(width: UIScreen.main.bounds.width, height: 500)
-			}
-			.padding(.horizontal, 4)
-			Text(entitys.comment)
-				.font(.system(size: 14))
-				.foregroundColor(Color.black)
-				.padding(.horizontal, 20)
-			Spacer()
+			
+			UploadUserProfileView()
+			
+			ImageView()
+			
+			CommentView()
 		}
 		.navigationTitle(
 			entitys.uploadTime
@@ -114,21 +57,16 @@ struct AlbumDetailView: View {
 			trailing:
 				Menu {
 					Button {
-						print("사진 저장")
 						alertType = .save
 					} label: {
 						Label("사진 저장", systemImage: "square.and.arrow.down")
 					}
-					
 					Button {
-						print("대표 이미지 설정")
 						alertType = .edit
 					} label: {
 						Label("대표 이미지 설정", systemImage: "star")
 					}
-					
 					Button(role: .destructive) {
-						print("삭제")
 						alertType = .remove
 					} label: {
 						Label("삭제", systemImage: "trash")
@@ -196,5 +134,91 @@ struct AlbumDetailView: View {
 				}
 			}
 		}
+	}
+}
+
+private extension AlbumDetailView {
+	
+	@ViewBuilder
+	func UploadUserProfileView() -> some View {
+		HStack() {
+			AsyncImage(url: URL(string: user.profileImage)) { image in
+				image
+					.resizable()
+					.frame(width: 32, height: 32)
+					.clipShape(Circle())
+			} placeholder: {
+				ProgressView()
+					.frame(width: 32, height: 32)
+			}
+			Text(user.nickname)
+				.font(.system(size: 12))
+				.fontWeight(.semibold)
+			Spacer()
+		}
+		.padding(.horizontal, 20)
+	}
+	
+	@ViewBuilder
+	func ImageView() -> some View {
+		AsyncImage(url: URL(string: entitys.url)) { image in
+			image
+				.resizable()
+				.frame(height: 500)
+				.cornerRadius(20)
+				.overlay {
+					HStack(alignment: .bottom) {
+						Divider().opacity(0)
+						Spacer()
+						VStack {
+							Circle()
+								.fill(Color.white)
+								.opacity(0.25)
+								.frame(width: 48, height: 48)
+								.overlay {
+									Button {
+										Task {
+											try await viewModel.actionLike(toggleChk: tempLikeState,
+																		   albumDocId: viewModel.albums.id,
+																		   fileName: entitys.fileName)
+											tempLikeCount = tempLikeState ? tempLikeCount - 1 : tempLikeCount + 1
+											tempLikeState.toggle()
+										}
+									} label: {
+										Image(systemName: "heart.fill")
+											.foregroundColor(tempLikeState ? Color.red : Color.white)
+									}
+								}
+							Rectangle()
+								.fill(Color.white)
+								.opacity(0.25)
+								.frame(width: 48, height: 24)
+								.cornerRadius(100)
+								.overlay {
+									Text("\(tempLikeCount)")
+										.font(.system(size: 16, weight: .medium))
+										.foregroundColor(Color.white)
+								}
+						}
+					}
+					.padding(.horizontal)
+					.padding(.bottom, 20)
+				}
+		} placeholder: {
+			ProgressView()
+				.frame(width: UIScreen.main.bounds.width, height: 500)
+		}
+		.padding(.horizontal, 4)
+	}
+	
+	@ViewBuilder
+	func CommentView() -> some View {
+		Spacer()
+			.frame(height: 16)
+		Text(entitys.comment)
+			.font(.system(size: 14))
+			.foregroundColor(Color.black)
+			.padding(.horizontal, 20)
+		Spacer()
 	}
 }
--- Yeowoo/Sources/Views/Album/Layouts/GalleryLayout.swift
+++ Yeowoo/Sources/Views/Album/Layouts/GalleryLayout.swift
@@ -22,8 +22,8 @@ struct GalleryLayout: View {
 			NavigationLink(destination:
 							AlbumDetailView(entitys: entitys[detailIndex],
 											user: self.user.first(where: {$0.docId == entitys[detailIndex].uploadUser}) ?? User(docId: "", id: "", email: "", password: "", isFirstLogin: false, nickname: "azhy", profileImage: "https://firebasestorage.googleapis.com/v0/b/yeowoo-186cd.appspot.com/o/album1%2F1.jpeg?alt=media&token=eeea845a-b7e0-4d77-a2d0-c3b30ca439e9", progressAlbum: "", finishedAlbum: [], notification: [], fcmToken: ""),
-											testBool: entitys[detailIndex].likeUsers.contains(UserDefaultsSetting.userDocId),
-											testCount: entitys[detailIndex].likeUsers.count,
+											tempLikeState: entitys[detailIndex].likeUsers.contains(UserDefaultsSetting.userDocId),
+											tempLikeCount: entitys[detailIndex].likeUsers.count,
 											viewModel: self.viewModel)
 						   ,isActive: $isActive
 			){
